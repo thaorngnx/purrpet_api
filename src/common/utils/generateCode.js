c8e22229ae32537975ee640327251d3f40337f62
Nguyen Thanh Phuong Thao <p.thao.21.602@gmail.com>
2023-10-22 19:56:31 +0700

    fix generateCode

--- src/common/utils/generateCode.js
+++ src/common/utils/generateCode.js
@@ -1,12 +1,13 @@
 import db from "../../models";
 
 export const generateCode = async (collectionName, prefix) => {
-    const collection = await db[collectionName].find().sort({ createAt: -1 }).limit(1);
-    const lastCode = collection.length > 0 ? collection[0].purrPetCode : "";
-    if (!lastCode) return prefix + "1";
+    const collection = await db[collectionName].find();
+    const lastCode = collection.length ? collection[collection.length - 1].purrPetCode : 0;
+    if (lastCode === 0) {
+        return (prefix + 1).toString();
+    }
     const lastNumber = lastCode.slice(prefix.length);
     const newNumber = (parseInt(lastNumber) + 1).toString();
     const newCode = prefix + newNumber;
-    console.log(newCode);
     return newCode;
 }

59261ec447330c3088b920f0f852b0fa7e73cca5
Nguyen Thanh Phuong Thao <86770002+thaorngnx@users.noreply.github.com>
2023-12-15 00:07:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,7 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  res.setHeader("Content-Type", "text/html");
-  next();
-});
+app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {

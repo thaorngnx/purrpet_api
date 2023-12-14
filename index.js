718a194262c4664670ee2c6beddb755f28ba4185
Nguyen Thanh Phuong Thao <86770002+thaorngnx@users.noreply.github.com>
2023-12-14 23:58:01 +0700

    text/html

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "text/html");
+  next();
+});
+
+
 app.get("/", (req, res) => {
   res.send("Welcome to PurrPet API");
 });

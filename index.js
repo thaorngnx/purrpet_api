842bf67b9572040c64a306e1752b1e2ea636d7d0
Nguyen Thanh Phuong Thao <86770002+thaorngnx@users.noreply.github.com>
2023-12-15 09:03:58 +0700

    disable cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    // origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
